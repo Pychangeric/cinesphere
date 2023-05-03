b5191c4e476004dd8a6847daf3b2f9b6a9ca0dc8
pychangeric <pychangeric@gmail.com>
2023-05-03 10:50:07 +0300

    update of Home.js and css

--- cinesphere/src/App.js
+++ cinesphere/src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import Home from './components/Home';
-import Categories from './components/Categories';
+
 
 function App() {
   return (
     
     <div className="App">
-      <Categories/>
+      
       <Home/>
     </div>
   );
