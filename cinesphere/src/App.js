6606879254922fb4e8f23e8efde5dbb01a10eaca
dMuguti <omiahderek56@gmail.com>
2023-05-04 09:19:33 +0300

    don with DOwnloads

--- cinesphere/src/App.js
+++ cinesphere/src/App.js
@@ -1,8 +1,19 @@
 import './App.css';
+import Home from './components/Home';
+import Add from './components/Add';
+import Downloads from './components/Downloads';
+
+
 
 function App() {
   return (
+    
     <div className="App">
+      <Downloads/>
+      <Add/>
+      <Home/>
+      
+     
     </div>
   );
 }
